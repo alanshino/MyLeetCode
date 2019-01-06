531e15c495e28f1fc6d38d94ef9db16ea9d58d1c
alanshino <alanwalker@gms.tku.edu.tw>
2019-01-06 13:39:29 +0800

    modify

--- 896. Monotonic Array.c
+++ 896. Monotonic Array.c
@@ -3,9 +3,9 @@ bool isMonotonic(int* A, int ASize) {
     if (ASize<=1){
         return 1;
     }
-    for (i=0;i<ASize-1;i++){
+    for (i=0;i<ASize-1;i++){             // find i <= j, A[i] >= A[j]
         if (*(A+i)>=*(A+i+1)){
-            continue;   
+            continue;
         }else{
             add++;
             break;
@@ -13,8 +13,8 @@ bool isMonotonic(int* A, int ASize) {
     }
     if (add==0) return 1;
     for (i=0;i<ASize-1;i++){
-        if (*(A+i)<=*(A+i+1)){
-            continue;   
+        if (*(A+i)<=*(A+i+1)){          // find i <= j, A[i] <= A[j]
+            continue;
         }else{
             add++;
             break;
